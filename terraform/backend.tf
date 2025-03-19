0c3b0e70dde9f0b42883bf7e8c86a8df0091e1b7
Sophie Kearney <78975142+sophie-kearney@users.noreply.github.com>
2025-03-19 10:34:08 -0400

    Revert "Initialize terraform and make bucket"

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket        = "bmin5100-terraform-state"
-    key           = "Sophie.Kearney@Pennmedicine.upenn.edu-Kearney_NeuroKB-DR/terraform.tfstate"
-    region        = "us-east-1"
-    encrypt       = true
-  }
-}
